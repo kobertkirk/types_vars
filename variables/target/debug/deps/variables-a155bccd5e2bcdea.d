f76f11ec2c8e59715939a377b3420c138b58aa36
kobert <none@email.com>
2025-01-13 21:45:16 -0600

    main

---
+++ variables/target/debug/deps/variables-a155bccd5e2bcdea.d
@@ -0,0 +1,5 @@
+/home/kobert/learning_rust/rust/3.1 Variables/variables/target/debug/deps/libvariables-a155bccd5e2bcdea.rmeta: src/main.rs
+
+/home/kobert/learning_rust/rust/3.1 Variables/variables/target/debug/deps/variables-a155bccd5e2bcdea.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/deps/variables-d8a45f7f2189f133.d
@@ -0,0 +1,5 @@
+/home/kobert/learning_rust/rust/3.1 Variables/variables/target/debug/deps/libvariables-d8a45f7f2189f133.rmeta: src/main.rs
+
+/home/kobert/learning_rust/rust/3.1 Variables/variables/target/debug/deps/variables-d8a45f7f2189f133.d: src/main.rs
+
+src/main.rs:
